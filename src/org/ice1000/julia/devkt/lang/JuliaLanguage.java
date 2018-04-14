b81a6d7407b2dd4d120dd11d156f2ae0da88c202
ice1000 <ice1000kotlin@foxmail.com>
2018-04-14 19:24:46 +0800

    Add parser

--- src/org/ice1000/julia/devkt/lang/JuliaLanguage.java
+++ src/org/ice1000/julia/devkt/lang/JuliaLanguage.java
@@ -1,8 +1,10 @@
 package org.ice1000.julia.devkt.lang;
 
-import org.jetbrains.kotlin.com.intellij.lang.Language;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.com.intellij.lang.Language;
+import org.jetbrains.kotlin.com.intellij.openapi.fileTypes.LanguageFileType;
 
 import static org.ice1000.julia.devkt.lang.Julia_constantsKt.JULIA_EXTENSION;
 import static org.ice1000.julia.devkt.lang.Julia_constantsKt.JULIA_LANGUAGE_NAME;
@@ -21,13 +23,22 @@ import static org.ice1000.julia.devkt.lang.Julia_constantsKt.JULIA_LANGUAGE_NAME
  * @author zxj5470
  */
 public final class JuliaLanguage extends Language {
-	public static final @NotNull JuliaLanguage INSTANCE = new JuliaLanguage();
+	public static final @NotNull
+	JuliaLanguage INSTANCE = new JuliaLanguage();
 
 	private JuliaLanguage() {
 		super(JULIA_LANGUAGE_NAME, "text/" + JULIA_EXTENSION);
 	}
 
-	@Override @Contract(pure = true) public boolean isCaseSensitive() {
+	@Override
+	public @Nullable
+	LanguageFileType getAssociatedFileType() {
+		return JuliaFileType.INSTANCE;
+	}
+
+	@Override
+	@Contract(pure = true)
+	public boolean isCaseSensitive() {
 		return false;
 	}
 }
