5a8eff1ad71957807afa626374319635c0f7d0d8
ice1000 <ice1000@sbrella.com>
2018-04-16 11:06:23 +0800

    Update docfmt

--- src/org/ice1000/julia/devkt/lang/docfmt/DocfmtLanguage.java
+++ src/org/ice1000/julia/devkt/lang/docfmt/DocfmtLanguage.java
@@ -3,6 +3,7 @@ package org.ice1000.julia.devkt.lang.docfmt;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.com.intellij.lang.Language;
+import org.jetbrains.kotlin.com.intellij.openapi.fileTypes.LanguageFileType;
 
 import static org.ice1000.julia.devkt.lang.Julia_constantsKt.DOCFMT_EXTENSION;
 import static org.ice1000.julia.devkt.lang.Julia_constantsKt.DOCFMT_LANGUAGE_NAME;
@@ -13,16 +14,17 @@ import static org.ice1000.julia.devkt.lang.Julia_constantsKt.DOCFMT_LANGUAGE_NAM
  * @author ice1000
  */
 public final class DocfmtLanguage extends Language {
-	public static final @NotNull
-	DocfmtLanguage INSTANCE = new DocfmtLanguage();
+	public static final @NotNull DocfmtLanguage INSTANCE = new DocfmtLanguage();
 
 	private DocfmtLanguage() {
 		super(DOCFMT_LANGUAGE_NAME, "text/" + DOCFMT_EXTENSION);
 	}
 
-	@Override
-	@Contract(pure = true)
-	public boolean isCaseSensitive() {
+	@Override public @NotNull LanguageFileType getAssociatedFileType() {
+		return DocfmtFileType.INSTANCE;
+	}
+
+	@Override @Contract(pure = true) public boolean isCaseSensitive() {
 		return true;
 	}
 }
--- src/org/ice1000/julia/devkt/lang/docfmt/docfmt-devkt.kt
+++ src/org/ice1000/julia/devkt/lang/docfmt/docfmt-devkt.kt
@@ -1,6 +1,7 @@
 package org.ice1000.julia.devkt.lang.docfmt
 
 import org.ice1000.devkt.openapi.*
+import org.ice1000.julia.devkt.lang.DOCFMT_EXTENSION
 import org.ice1000.julia.devkt.lang.docfmt.psi.DocfmtValue
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 
@@ -9,9 +10,13 @@ class Docfmt<T> : ExtendedDevKtLanguage<T>(
 		DocfmtParserDefinition
 ) {
 	override val lineCommentStart get() = "//"
+	override fun satisfies(fileName: String) = fileName == ".$DOCFMT_EXTENSION"
 	override fun annotate(element: PsiElement, document: AnnotationHolder<T>, colorScheme: ColorScheme<T>) {
 		super.annotate(element, document, colorScheme)
 		if (element is DocfmtValue) {
+			val text = element.text
+			if (text == "true" || text == "false") document.highlight(element, colorScheme.keywords)
+			else if (text.all { it.isDigit() }) document.highlight(element, colorScheme.numbers)
 		}
 	}
 }
